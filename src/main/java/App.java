6c583b9d1cdbe4e38fbb8fb9086876afedeef325
Drujishe <homandru@yandex.ru>
2023-01-22 14:40:34 +0300

    first

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,13 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld world1 = applicationContext.getBean(HelloWorld.class);
+        HelloWorld world2 = applicationContext.getBean(HelloWorld.class);
+        System.out.println( "HelloWorld's link equals - " + (world1 == world2) );
+
+        Cat cat1 = applicationContext.getBean(Cat.class);
+        Cat cat2 = applicationContext.getBean(Cat.class);
+        System.out.println( "Cat's link equals - " + (cat1 == cat2) );
     }
 }
--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -2,13 +2,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
+import java.util.Random;
+
 @Configuration
 public class AppConfig {
- 
-    @Bean(name="helloworld")
+
+    @Bean(name = "helloworld")
+    @Scope("singleton")
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
+    }
+
+    @Bean(name = "cat")
+    @Scope("prototype")
+    public Cat getCat() {
+        Cat cat = new Cat();
+        cat.setAge((byte) (new Random()).nextInt(0, 15));
+        return cat;
     }
 }
